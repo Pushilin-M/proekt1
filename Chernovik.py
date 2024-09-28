852ab49b1f7ba9629a345029a304777a3c4e94ce
Pushilin-M <mvpooh@gmail.com>
2024-09-28 10:00:40 +0300

    Add files via upload

---
+++ Chernovik.py
@@ -0,0 +1,22 @@
+
+
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+
+    list_ = ['.com', '.net', '.ru']
+    for i in list_:
+        recipient.endswith(i) or recipient.endswith(i) or recipient.endswith(i)
+        break
+    else:
+        print('-***-')
+        for j in list_:
+            sender.endswith(j) or sender.endswith(j) or sender.endswith(j)
+            break
+        else:
+            print('*---*')
+    print('jhjjjgjhjhjhjh')
+
+
+    send_email('Проверка работы кода', 'vasyok1337@gmail.com')
+    send_email('Проверка работы кода', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+    send_email('Проверка работы кода', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+    send_email('Проверка работы кода', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
